4b95bcaf272e6229da7e3234d363b1784beebac3
valkiii <valmirmustafa2005@gmail.com>
2024-12-20 15:14:40 +0100

    updated tables with informtion and added button

---
+++ flights.js
@@ -0,0 +1,25 @@
+$(function(){
+    $("#navbar-placeholder").load("nav.html");
+    $("#footer-placeholder").load("footer.html");
+})
+
+$(document).ready(function(){
+    $(".filter-item").click(function(){
+        const value = $(this).attr("data-filter");
+        if (value == "all") {
+            $(".post-box").show("3000");
+        } else{
+            $(".post-box")
+                .not("." + value)
+                .hide("3000"); 
+            $(".post-box")
+                .filter("." + value)
+                .show("3000");
+        }   
+    });
+
+    //Active filterin
+    $(".filter-item").click(function(){
+        $(this).addClass("active-filter").siblings().removeClass("active-filter")
+    })
+})
