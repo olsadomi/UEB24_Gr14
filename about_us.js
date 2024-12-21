4d26c147ac3d89913295650c5fe9c5c358628a87
Rreze Ejupi <rrezeejupi@gmail.com>
2024-12-22 00:25:57 +0100

    newsletter popup

--- about_us.js
+++ about_us.js
@@ -1,4 +1,21 @@
-$(function(){
+$(function () {
     $("#navbar-placeholder").load("nav.html");
     $("#footer-placeholder").load("footer.html");
+});
+
+
+function popupFn() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("popupDialog").style.display = "block";
+}
+
+function closeFn() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popupDialog").style.display = "none";
+}
+
+document.getElementById("overlay").addEventListener("click", function (e) {
+    if (e.target === this) {
+        closeFn();
+    }
 });
